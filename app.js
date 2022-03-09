25b3abbeebf9fab47a5fbe2bb5815492457cb9ae
Soha Kounine <konainsoha@gmail.com>
2022-03-09 19:23:21 +0530

    static rpn_calculator

---
+++ app.js
@@ -0,0 +1,38 @@
+function reversePolish(newExpr) {
+    let expr = newExpr.split(" ");
+    let stack = [];
+    if (expr === '') {
+        return 0;
+    }
+    for (let i = 0; i < expr.length; i++) {
+        if (!isNaN(expr[i]) && isFinite(expr[i])) {
+            stack.push(expr[i]);
+
+        } else {
+            let a = stack.pop();
+            let b = stack.pop();
+            if (expr[i] === "+") {
+                stack.push(parseInt(a) + parseInt(b));
+            } else if (expr[i] === "-") {
+                stack.push(parseInt(b) - parseInt(a));
+            } else if (expr[i] === "*") {
+                stack.push(parseInt(a) * parseInt(b));
+            } else if (expr[i] === "/") {
+                stack.push(parseInt(b) / parseInt(a));
+            } else if (expr[i] === "^") {
+                stack.push(Math.pow(parseInt(b), parseInt(a)));
+            } else if (expr[i] === "q") {
+                process.exit()
+
+            }
+        }
+    }
+
+    if (stack.length > 1) {
+        return "ERROR";
+    } else {
+        return stack[0];
+    }
+}
+
+console.log(reversePolish('2 4 6 * +'));
