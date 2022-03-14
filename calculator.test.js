b1b25445c1bf2ab989786a35d6add8ba306af8e9
Soha Kounine <konainsoha@gmail.com>
2022-03-14 17:05:08 +0530

    rpn_calculator

--- calculator.test.js
+++ calculator.test.js
@@ -2,6 +2,6 @@ const calculator = require('./app.js');
 
 test('Two or more numbers should return the respective calculated operation provided', () => {
 
-    expect(calculator.reverse_polish_notation('6,3,*')).toBe(15);
+    expect(calculator.reverse_polish_notation('6,3,*')).toBe(18);
 
 });
